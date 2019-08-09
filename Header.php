de3b9291553afafd06bac5e3f36a5ad0b6595b26
Paulo Victor viola <paulovictorviola@gmail.com>
2019-08-08 21:54:01 -0300

    Criando pagina de cadastro

---
+++ Header.php
@@ -0,0 +1,7 @@
+<div class="card card-body bg-dark">
+        <h3 class="text-white">Projeto de Controle de Clientes</h3>
+        <p class="text-white">
+            Sistema desenvolvido em PHP e MySQL
+        </p>
+    </div>
+    <br>
--- index.php
+++ index.php
@@ -2,9 +2,27 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <title>Document</title>
 </head>
 <body>
-    
+    <div class="card card-body bg-dark">
+        <h3 class="text-white">Projeto de Controle de Clientes</h3>
+        <p class="text-white">
+            Sistema desenvolvido em PHP e MySQL
+        </p>
+    </div>
+    <br>
+    <div class="container">
+        <h2>Painel do Sistema</h2>
+        <hr>
+        <h4>Menu</h4>
+        <nav>
+            <ul>
+                <li><a href="cadastrar.php">Cadastro de Clientes</a></li>
+                <li><a href="consultar.php">Consulta de Clientes</a></li>
+            </ul>
+        </nav>
+    </div>
 </body>
 </html>
